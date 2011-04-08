437118c1beb533ca2514a589eed755bdc9456f2e
Philipp Brumm <philipp@funkensturm.de>
2011-04-08 19:22:13 +0200

    mostly animating, comment loading?

--- reddit.js
+++ reddit.js
@@ -4,7 +4,7 @@ var Post = Backbone.Model.extend({
 
 var Posts = Backbone.Collection.extend({
    model: Post, 
-   url: "http://www.reddit.com/hot/.json?limit=100&jsonp=?", 
+   url: "http://www.reddit.com/.json?feed=073632c88f054fc2e478e09f3f6016494066b752&user=cmdq&limit=100&jsonp=?", 
    initialize: function() {
    },
    parse: function(response) {
@@ -13,6 +13,7 @@ var Posts = Backbone.Collection.extend({
          child.data.thumbnail = _.isEmpty(child.data.thumbnail) || child.data.thumbnail.match(/^\/static/) ? "noimage.png" : child.data.thumbnail;
          child.data.thumbnail = child.data.image_url ? child.data.image_url : child.data.thumbnail;
          child.data.permalink = "http://www.reddit.com" + child.data.permalink;
+         child.data.flashcontent = child.data.url.match(/youtube/) ? "http://www.redditmedia.com/mediaembed/" + child.data.id : null;
          return child.data;
       });
       return parsed;
@@ -31,7 +32,13 @@ var PostView = Backbone.View.extend({
          image_url : this.model.get("image_url"),
          selftext : $("<div></div>").html(this.model.get("selftext_html")).text(),
          url : this.model.get("url"),
-         permalink : this.model.get("permalink")
+         permalink : this.model.get("permalink"),
+         flashcontent : this.model.get("flashcontent"),
+         subreddit : this.model.get("subreddit"),
+         author : this.model.get("author"),
+         num_comments : this.model.get("num_comments"),
+         ups : this.model.get("ups"),
+         downs : this.model.get("downs")
       });
       return this;
    }
@@ -43,8 +50,7 @@ var AppView = Backbone.View.extend({
       _.bindAll(this, "render", "addPost", "addAllPosts");
       if (this.options.subreddit) {
          this.posts = new Posts();
-         this.posts.url = "http://www.reddit.com/r/" + this.options.subreddit + "/.json?jsonp=?";
-         console.log(this.posts.url);
+         this.posts.url = "http://www.reddit.com/r/" + this.options.subreddit + "/.json?limit=100&jsonp=?";
       } 
       else {
          this.posts = new Posts();
@@ -55,6 +61,15 @@ var AppView = Backbone.View.extend({
       
       this.posts.fetch();
       this.render();
+   },
+   events: {
+   "change #search": "loadSubreddit"
+   },
+   loadSubreddit: function(event) {
+      var query = $(event.currentTarget).val();
+      this.posts.url = "http://www.reddit.com/r/" + query + "/.json?limit=100&jsonp=?";
+      this.render();
+      this.posts.fetch();
    },
    render: function() {
       $(this.el).find("#posts .post").remove();
@@ -93,37 +108,39 @@ $(document).ready(function() {
    Backbone.history.start();
    
    $(".post_actions li").live("click", function(event) {
-      var target = $(this).attr("class").split("_")[0];
+      var target = $(this);
+      
+      if (!target.attr("class").match(/button/)) {
+         return;
+      };
+      
+      var targetType = target.attr("class").split("_")[0];
       var post = $(this).closest(".post");
-      var wanted = post.find("span." + target);
+      var wanted = post.find("." + $(this).attr("class").split("_")[0]);
       this.old_height = this.height;
       this.height = post.height();
       post.css('height', this.height);
-      console.log(this.height);
       event.preventDefault();
-      
       
       $(this).addClass('clicked');
       
-      if (target == "comments") {
+      if (targetType == "comments") {
          if (post.hasClass("open")) {
-            post.animate({ "height": this.old_height });
-            post.removeClass("open");
-            $(this).removeClass('clicked');
-            post.find(".comment").remove();
+            post.animate({"background-color": "#E8E7E2", "height": this.old_height }, "", "easeOutQuart", function() {
+               post.removeClass("open");
+               target.removeClass('clicked');
+               post.find(".comment").remove();
+            });
          } 
          else {
-            $.getJSON("http://www.reddit.com/comments/" + post.attr("id") + ".json?limit=4&jsonp=?", function(response) {
+            $.getJSON("http://www.reddit.com/comments/" + post.attr("id") + ".json?limit=5&jsonp=?", function(response) {
                for (var i=0; i < response[1].data.children.length-1; i++) {
                   var comment = response[1].data.children[i].data;
                   post.find(".content").append($("<span id='" + comment.id + "' class='comment'></span>")); 
                   post.find("#" + comment.id).append($("<div></div>").html(comment.body_html).text());
-                  //.append(response[1].data.children[i].data.body_html).text());
                };
                post.addClass("open");
-               post.animate({
-                  height: post.find(".content").height()
-               });
+               post.animate({"background-color": "#7E7565", "height" : post.find(".content").height() }, "", "easeOutQuart");
             });
             return;
          };
@@ -131,11 +148,15 @@ $(document).ready(function() {
       if (wanted.hasClass("hidden")) {
          wanted.removeClass('hidden');
          post.addClass("open");
+         // post.animate({"background-color": "#7E7565", "height" : post.find(".content").height() > 400 ? "400px" : post.find(".content").height() });
+         post.animate({"background-color": "#7E7565", "height" : post.find(".content").height() }, "", "easeOutQuart");
       } 
       else {
-         wanted.addClass('hidden');
-         post.removeClass("open");
-         $(this).removeClass('clicked');
+         post.animate({"background-color": "#E8E7E2", "height": this.old_height }, "", "easeOutQuart", function() {
+            wanted.addClass('hidden');
+            post.removeClass("open");
+            target.removeClass('clicked');
+         });
       };
    });
 });
